df622f4ec6824bcdf14d9d8dd173dec5b3f88e84
Mauricio <rinripper@gmail.com>
2021-07-25 14:29:20 -0400

    audio clean

--- mplab_audio/space-invaders.X/main.c
+++ mplab_audio/space-invaders.X/main.c
@@ -38,63 +38,24 @@ int main(void)
     // Initialize the device
     SYSTEM_Initialize();
     OC1_Start();
-    //TMR2_Start();
     INTERRUPT_GlobalEnable();
-    //TMR2_SetInterruptHandler( &mio);
-    /*--------------------------------------------------------------------------
-     Component Declarations
-	--------------------------------------------------------------------------*/
-    
-    
+
+
     /*--------------------------------------------------------------------------
      Component Initialization
 	--------------------------------------------------------------------------*/
     //bool completeCycle;
     //unsigned short index = 0;
-    long int frame;
-    long int ufito;
-    unsigned short index = 0;
-    /*--------------------------------------------------------------------------
-     Game Loop - Design Pattern
-     * 
-     * A game loop runs continuously during gameplay. Each turn of the loop, it
-     * processes user input without blocking, updates the game state, and 
-     * renders the game. 
-     * It tracks the passage of time to control the rate of gameplay.
-	--------------------------------------------------------------------------*/
-    
+    //long int frame;
+
+
     
     while (1)
     {
-        //elapsed = (char)(currentTick - previousTick); 
-        //previousTick = currentTick;
-        //lag += elapsed;
-        
-        /*----------------------------------------------------------------------
-         Inputs
-        ----------------------------------------------------------------------*/
-        
-        //spaceship.attack(&spaceship, &spaceship_bullet);
-        
-        /*----------------------------------------------------------------------
-         Updates
-        ----------------------------------------------------------------------*/
-        //spaceship.update(&spaceship, elapsed);
-        //mothership.update(&mothership, elapsed);
-        //invaders_alive.update(&invaders_alive, &gameMap, elapsed);
-        //spaceship_bullet.update(&spaceship_bullet, elapsed);
-        //gameMap.detectColision(&gameMap, &spaceship_bullet);
 
-        /*----------------------------------------------------------------------
-         Render
-        ----------------------------------------------------------------------*/
-        //index += 1000;
-        //OC1_Tasks();
-       // frame = PR2;
-        //ufito = UFO[index++ % (NUM_ELEMENTS_UFO)];
-        //frame *= ufito;
+        //frame = PR2;
+        //frame *= UFO[index++ % (NUM_ELEMENTS_UFO)];
         //frame /= 255;
-        //frame = index++ % (PR2);
         //OC1_SecondaryValueSet(frame);
         
     }
